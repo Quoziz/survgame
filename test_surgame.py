5e2106e267927439b9e2e54a41325fe98970f488
Quoziz <93032242+Quoziz@users.noreply.github.com>
2022-12-23 11:27:08 +0300

    Add files via upload

---
+++ test_surgame.py
@@ -0,0 +1,11 @@
+import unittest
+import pygame
+pygame.init()
+import surgame
+class SurvivalgameTest(unittest.TestCase):
+    def test_drawWindow(self):
+        self.assert(closest_pair_of_points([(1, 1), (2, 2)]),(1, 1), (2, 2))
+
+
+
+
